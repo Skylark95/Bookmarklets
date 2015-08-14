62c0b4ea51c00e325503bbfff546421f07fe4bfa
Derek <derek@skylark95.com>
2015-08-13 21:19:47 -0500

    Add Ninite, minify js

---
+++ Ninite.js
@@ -0,0 +1,9 @@
+var span = document.getElementsByTagName('span'),
+    result = document.getElementsByClassName('didyouknow')[0],
+    apps = '<h2 style="margin-bottom:1em;">Installer includes:</h2>';
+for(var i = 0; i < span.length; i++) {
+    if (span[i].title) {
+        apps += '<p style="margin:0;">' + span[i].title + '</p>';
+    }
+}
+result.innerHTML = apps;
